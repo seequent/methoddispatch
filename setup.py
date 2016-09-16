51f40723d44374484c934cfb7d40d34016a26612
Tim Mitchell <tim.mitchell@leapfrog3d.com>
2016-09-16 16:07:25 +1200

    udate url

--- setup.py
+++ setup.py
@@ -11,8 +11,8 @@ setuptools.setup(
     author_email = "tim.mitchell@leapfrog3d.com",
     description = "singledispatch decorator for instance methods.",
     license = "BSD",
-    keywords = "single dispatch decorator",
-    url = "http://packages.python.org/methoddispatch",
+    keywords = "single dispatch decorator method",
+    url = "https://github.com/tim-mitchell/methoddispatch",
     packages = setuptools.find_packages(),
     long_description=methoddispatch.__doc__,
     classifiers=[
