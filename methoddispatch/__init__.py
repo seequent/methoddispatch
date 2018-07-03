4c865f4756c8e8b7a995a2b68a13bfd41be41ef5
Tim Mitchell <tim.mitchell@aranzgeo.com>
2018-07-03 15:05:48 +1200

    All changes for 3.7 work on 3.6 so changing version at which they are enabled.

--- methoddispatch/__init__.py
+++ methoddispatch/__init__.py
@@ -178,9 +178,9 @@ import sys
 if sys.version_info[0] < 3:
     from .methoddispatch2 import *
     del methoddispatch2
-elif sys.version_info >= (3, 7):
-    from .methoddispatch37 import *
-    del methoddispatch37
+elif sys.version_info >= (3, 6):
+    from .methoddispatch36 import *
+    del methoddispatch36
 else:
     from .methoddispatch3 import *
     del methoddispatch3
